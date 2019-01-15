c6388a3c89a540ef680a7bb00327f1270712863d
Emily Dela Cruz <delacruz.emily@gmail.com>
2019-01-15 06:09:43 -0500

    Change router base

--- src/main.js
+++ src/main.js
@@ -34,7 +34,8 @@ Vue.use(VueRouter);
 const router = new VueRouter({
 	routes, // Have to pass an object where we setup the router. ES6 syntax (routes:routes)
 	mode: 'history', // Remove the hash
-	base: '/boursesdeparc',
+	// base: '/boursesdeparc',
+	base: '/grantsfr2019',
 	scrollBehavior (to, from, savedPosition) {
 		if (to.hash) {
 			console.log('HASH');
