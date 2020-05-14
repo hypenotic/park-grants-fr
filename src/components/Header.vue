340386a2635bb277134271966e71a08c8d47cbc1
Hubert <hubhcsi@gmail.com>
2020-05-14 11:28:09 -0400

    add recipients back to nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,8 +16,8 @@
         <div id="navbarExampleTransparentExample" class="navbar-menu" v-bind:class="{ 'menu-open': showMobileMenu }">
             <div class="navbar-end">
                 <a href="https://parkpeople.ca" class="navbar-item">Page d'accueil</a>
-                <!-- <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path == '/recipiendaires-de-bourses'"><router-link to="/" exact>Bourses</router-link></span>
-                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path == '/'"><router-link to="/recipiendaires-de-bourses"  exact>Récipiendaires de bourses</router-link></span> -->
+                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path == '/recipiendaires-de-bourses'"><router-link to="/" exact>Bourses</router-link></span>
+                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path == '/'"><router-link to="/recipiendaires-de-bourses"  exact>Récipiendaires de bourses</router-link></span>
             </div>
         </div>
     </nav>
@@ -38,8 +38,8 @@
         <div id="navbarExampleTransparentExample" class="navbar-menu" v-bind:class="{ 'menu-open': showMobileMenu }">
             <div class="navbar-end">
                 <a href="https://parkpeople.ca" class="navbar-item">Page d'accueil</a>
-                <!-- <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path == '/recipiendaires-de-bourses'"><router-link to="/" exact>Bourses</router-link></span>
-                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path == '/'"><router-link to="/recipiendaires-de-bourses"  exact>Récipiendaires de bourses</router-link></span> -->
+                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path == '/recipiendaires-de-bourses'"><router-link to="/" exact>Bourses</router-link></span>
+                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path == '/'"><router-link to="/recipiendaires-de-bourses"  exact>Récipiendaires de bourses</router-link></span>
             </div>
         </div>
     </nav>
