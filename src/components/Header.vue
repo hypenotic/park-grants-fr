e375eedd66b2479936251682be7508521cf424b6
Lionel Mann <lionelmann@gmail.com>
2021-02-24 10:48:09 -0500

    change nav label

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,233 +1,287 @@
 <template>
-    <nav v-if="scrolled == true" id="scrolling" class="navbar green">
-        <div class="navbar-brand">
-            <div>
-                <a href="https://parkpeople.ca/boursesdeparc2019"><img src="https://parkpeople.ca/custom/uploads/2019/04/TD_logo_french_-white_nologos.png" class="grants-logo"></a>
-            </div>
-        </div>
-        <div class="lang">
-            <a v-if="this.$route.path == '/faq'" href="https://parkpeople.ca/parkgrants/faq">EN</a>
-            <a v-else href="https://parkpeople.ca/parkgrants">EN</a>
-            {{ lang }}
-        </div>
-        <div id="mobile-menu-trigger" v-on:click="showMobileMenu = !showMobileMenu">
-            <i class="fa fa-bars" aria-hidden="true"></i>
-        </div>
-        <div id="navbarExampleTransparentExample" class="navbar-menu" v-bind:class="{ 'menu-open': showMobileMenu }">
-            <div class="navbar-end">
-                <a href="https://parkpeople.ca/?lang=fr" class="navbar-item">Page d'accueil</a>
-                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path == '/recipiendaires-de-bourses'"><router-link to="/" exact>Bourses</router-link></span>
-                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path == '/'"><router-link to="/recipiendaires-de-bourses"  exact>Récipiendaires de bourses</router-link></span>
-            </div>
-        </div>
-    </nav>
-    <nav v-else class="navbar green">
-        <div class="navbar-brand">
-            <div>
-                <a href="https://parkpeople.ca/boursesdeparc"><img src="https://parkpeople.ca/custom/uploads/2019/04/TD_logo_french_-white_nologos.png" class="grants-logo"></a>
-            </div>
-        </div>
-        <div class="lang">
-            <a v-if="this.$route.path == '/faq'" href="https://parkpeople.ca/parkgrants/faq">EN</a>
-            <a v-else href="https://parkpeople.ca/parkgrants">EN</a>
-            {{ lang }}
-        </div>
-        <div id="mobile-menu-trigger" v-on:click="showMobileMenu = !showMobileMenu">
-            <i class="fa fa-bars" aria-hidden="true"></i>
-        </div>
-        <div id="navbarExampleTransparentExample" class="navbar-menu" v-bind:class="{ 'menu-open': showMobileMenu }">
-            <div class="navbar-end">
-                <a href="https://parkpeople.ca/?lang=fr" class="navbar-item">Page d'accueil</a>
-                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path == '/recipiendaires-de-bourses'"><router-link to="/" exact>Bourses</router-link></span>
-                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path == '/'"><router-link to="/recipiendaires-de-bourses"  exact>Récipiendaires de bourses</router-link></span>
-            </div>
-        </div>
-    </nav>
+  <nav v-if="scrolled == true" id="scrolling" class="navbar green">
+    <div class="navbar-brand">
+      <div>
+        <a href="https://parkpeople.ca/boursesdeparc2019"
+          ><img
+            src="https://parkpeople.ca/custom/uploads/2019/04/TD_logo_french_-white_nologos.png"
+            class="grants-logo"
+        /></a>
+      </div>
+    </div>
+    <div class="lang">
+      <a
+        v-if="this.$route.path == '/faq'"
+        href="https://parkpeople.ca/parkgrants/faq"
+        >EN</a
+      >
+      <a v-else href="https://parkpeople.ca/parkgrants">EN</a>
+      {{ lang }}
+    </div>
+    <div id="mobile-menu-trigger" v-on:click="showMobileMenu = !showMobileMenu">
+      <i class="fa fa-bars" aria-hidden="true"></i>
+    </div>
+    <div
+      id="navbarExampleTransparentExample"
+      class="navbar-menu"
+      v-bind:class="{ 'menu-open': showMobileMenu }"
+    >
+      <div class="navbar-end">
+        <a href="https://parkpeople.ca/?lang=fr" class="navbar-item"
+          >Page d'accueil</a
+        >
+        <span
+          class="navbar-item"
+          v-on:click="showMobileMenu = !showMobileMenu"
+          v-if="this.$route.path == '/recipiendaires-de-bourses'"
+          ><router-link to="/" exact
+            >Récipiendaires de bourses</router-link
+          ></span
+        >
+        <span
+          class="navbar-item"
+          v-on:click="showMobileMenu = !showMobileMenu"
+          v-if="this.$route.path == '/'"
+          ><router-link to="/recipiendaires-de-bourses" exact
+            >Récipiendaires de bourses</router-link
+          ></span
+        >
+      </div>
+    </div>
+  </nav>
+  <nav v-else class="navbar green">
+    <div class="navbar-brand">
+      <div>
+        <a href="https://parkpeople.ca/boursesdeparc"
+          ><img
+            src="https://parkpeople.ca/custom/uploads/2019/04/TD_logo_french_-white_nologos.png"
+            class="grants-logo"
+        /></a>
+      </div>
+    </div>
+    <div class="lang">
+      <a
+        v-if="this.$route.path == '/faq'"
+        href="https://parkpeople.ca/parkgrants/faq"
+        >EN</a
+      >
+      <a v-else href="https://parkpeople.ca/parkgrants">EN</a>
+      {{ lang }}
+    </div>
+    <div id="mobile-menu-trigger" v-on:click="showMobileMenu = !showMobileMenu">
+      <i class="fa fa-bars" aria-hidden="true"></i>
+    </div>
+    <div
+      id="navbarExampleTransparentExample"
+      class="navbar-menu"
+      v-bind:class="{ 'menu-open': showMobileMenu }"
+    >
+      <div class="navbar-end">
+        <a href="https://parkpeople.ca/?lang=fr" class="navbar-item"
+          >Page d'accueil</a
+        >
+        <span
+          class="navbar-item"
+          v-on:click="showMobileMenu = !showMobileMenu"
+          v-if="this.$route.path == '/recipiendaires-de-bourses'"
+          ><router-link to="/" exact
+            >Récipiendaires de bourses</router-link
+          ></span
+        >
+        <span
+          class="navbar-item"
+          v-on:click="showMobileMenu = !showMobileMenu"
+          v-if="this.$route.path == '/'"
+          ><router-link to="/recipiendaires-de-bourses" exact
+            >Récipiendaires de bourses</router-link
+          ></span
+        >
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
-import { eventBus } from '../main.js';
+import { eventBus } from "../main.js";
 export default {
-    data() {
-        return {
-            id: this.$route.params.id,
-            lang: '',
-            showMobileMenu: false,
-            scrolled: false
-        }
-    },
-    methods: {
-        handleScroll: function (event) {
-            if (window.addEventListener){
-                // console.log('A');
-                // console.log(pageYOffset);
-                if (window.pageYOffset > 20) {
-                    this.scrolled = true;
-                } else {
-                    this.scrolled = false;	
-                }
-            } else if (window.attachEvent){
-                // console.log('B');
-                if (window.scrollY > 20) {
-                    this.scrolled = true;
-                } else {
-                    this.scrolled = false;	
-                }
-            } else {
-                if (window.pageYOffset > 20) {
-                    // console.log('C');
-                    this.scrolled = true;
-                } else {
-                    this.scrolled = false;	
-                }
-            }
-        }
-    },
-    created: function () {
-        // window.addEventListener('scroll', this.handleScroll);
-        if (window.addEventListener){
-            // console.log('Option 1');
-            window.addEventListener('scroll', this.handleScroll);
-        } else if (window.attachEvent){
-            // console.log('Option 2');
-            window.attachEvent('scroll', this.handleScroll);
+  data() {
+    return {
+      id: this.$route.params.id,
+      lang: "",
+      showMobileMenu: false,
+      scrolled: false,
+    };
+  },
+  methods: {
+    handleScroll: function (event) {
+      if (window.addEventListener) {
+        // console.log('A');
+        // console.log(pageYOffset);
+        if (window.pageYOffset > 20) {
+          this.scrolled = true;
         } else {
-            // console.log('Option 3');
-            jQuery(window).on('scroll', this.handleScroll);
-            // console.log('IE');
+          this.scrolled = false;
         }
-    },
-    destroyed: function () {
-        // window.removeEventListener('scroll', this.handleScroll);
-        if (window.addEventListener){
-            window.removeEventListener('scroll', this.handleScroll);
-        } else if (window.attachEvent){
-            window.detachEvent('scroll', this.handleScroll);
+      } else if (window.attachEvent) {
+        // console.log('B');
+        if (window.scrollY > 20) {
+          this.scrolled = true;
         } else {
-            window.removeEventListener('scroll', this.handleScroll);
+          this.scrolled = false;
         }
+      } else {
+        if (window.pageYOffset > 20) {
+          // console.log('C');
+          this.scrolled = true;
+        } else {
+          this.scrolled = false;
+        }
+      }
+    },
+  },
+  created: function () {
+    // window.addEventListener('scroll', this.handleScroll);
+    if (window.addEventListener) {
+      // console.log('Option 1');
+      window.addEventListener("scroll", this.handleScroll);
+    } else if (window.attachEvent) {
+      // console.log('Option 2');
+      window.attachEvent("scroll", this.handleScroll);
+    } else {
+      // console.log('Option 3');
+      jQuery(window).on("scroll", this.handleScroll);
+      // console.log('IE');
     }
+  },
+  destroyed: function () {
+    // window.removeEventListener('scroll', this.handleScroll);
+    if (window.addEventListener) {
+      window.removeEventListener("scroll", this.handleScroll);
+    } else if (window.attachEvent) {
+      window.detachEvent("scroll", this.handleScroll);
+    } else {
+      window.removeEventListener("scroll", this.handleScroll);
+    }
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-
-@import '../styles/variables.scss';
+@import "../styles/variables.scss";
 .green {
-    background-color: #067f1b;
+  background-color: #067f1b;
 }
 
 nav#scrolling {
-    @media #{$small-and-down} {
-		// display: block;
-		position: fixed; 
-        width: 100%;
-		z-index: 6000;
-    }
+  @media #{$small-and-down} {
+    // display: block;
+    position: fixed;
+    width: 100%;
+    z-index: 6000;
+  }
 }
 
 #mobile-menu-trigger {
-	display: none;
-	&:hover {
-		cursor: pointer;
-	}
-	i {
-		color: $white;
-		font-size: 30px;
-	}
-	@media #{$medium-and-down} {
-		display: block;
-		position: fixed; 
-		right: 30px;
-		top: 15px;
-		z-index: 5000;
-    }
-	
+  display: none;
+  &:hover {
+    cursor: pointer;
+  }
+  i {
+    color: $white;
+    font-size: 30px;
+  }
+  @media #{$medium-and-down} {
+    display: block;
+    position: fixed;
+    right: 30px;
+    top: 15px;
+    z-index: 5000;
+  }
 }
 
 .navbar {
+  z-index: 5000;
+  @media #{$small-and-down} {
+    display: flex;
+    align-items: center;
+    position: relative;
     z-index: 5000;
-    @media #{$small-and-down} {
-        display: flex;
-        align-items: center;
-        position: relative;
-        z-index: 5000;
-    }
+  }
 }
 
 .navbar-brand {
-    @media #{$small-and-down} {
-        display: inline-block;
-    }
+  @media #{$small-and-down} {
+    display: inline-block;
+  }
 }
 
 .navbar-menu {
-    a {
-        color: $white;
-        &:hover {
-            color: $blue;
-            background: $white;
-        }
+  a {
+    color: $white;
+    &:hover {
+      color: $blue;
+      background: $white;
     }
-    span {
-        &:hover {
-            background: $white;
-            color: $blue;
-            a {
-                color: $blue;
-            }
-        }
+  }
+  span {
+    &:hover {
+      background: $white;
+      color: $blue;
+      a {
+        color: $blue;
+      }
     }
+  }
 }
 
 .navbar-menu.menu-open {
-    @media #{$small-and-down} {
-        position: absolute;
-        display: inline-block;
-        top: 55px;
-        width: 100%;
-        background:  #067f1b;
-    }
+  @media #{$small-and-down} {
+    position: absolute;
+    display: inline-block;
+    top: 55px;
+    width: 100%;
+    background: #067f1b;
+  }
 }
 
 .lang {
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
+  color: white;
+  margin: 1em;
+  @media #{$small-and-down} {
+    display: inline-block;
+    margin: 0 0 0 16px;
+  }
+  a {
     color: white;
-    margin: 1em;
-    @media #{$small-and-down} {
-        display: inline-block;
-        margin: 0 0 0 16px;
+    &:hover {
+      font-weight: bold;
     }
-    a {
-        color: white;
-        &:hover {
-            font-weight: bold;
-        }
-    }
+  }
 }
 
 .navbar-item {
-    color: white;
+  color: white;
 }
 
 .grants-logo {
+  max-height: 50px;
+  width: auto;
+  margin-top: 3px;
+  margin-left: 8px;
+  @media #{$large-and-up} {
     max-height: 50px;
     width: auto;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    margin-left: 10px;
+  }
+  @media #{$xlarge-and-up} {
+    max-height: 70px;
+    width: auto;
     margin-top: 3px;
-    margin-left: 8px; 
-    @media #{$large-and-up} {
-		max-height: 50px;
-        width: auto;
-        margin-top: 10px;
-        margin-bottom: 10px;
-        margin-left: 10px; 
-	}
-    @media #{$xlarge-and-up} {
-		max-height: 70px;
-        width: auto;
-        margin-top: 3px;
-        margin-left: 8px; 
-	}
+    margin-left: 8px;
+  }
 }
 </style>
